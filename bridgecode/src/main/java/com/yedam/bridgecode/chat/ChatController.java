6c190f306ac88479c3ad820ff93f524a4990b5dc
User <User@User-PC>
2017-06-13 17:52:12 +0900

    커플채팅부분 commit.

--- bridgecode/src/main/java/com/yedam/bridgecode/chat/ChatController.java
+++ bridgecode/src/main/java/com/yedam/bridgecode/chat/ChatController.java
@@ -1,5 +1,13 @@
 package com.yedam.bridgecode.chat;
 
+import java.util.List;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +28,45 @@ public class ChatController {
 	ChatService chatService;
 	@Autowired
 	MemberService memberService;
+	
+	@RequestMapping("/chat/chatTranslate.do")
+	public void chatTranslate(String tt) {
+		
+		String clientId = "Yq8eXfIrImd6XBx6QhUs";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "xwZV9UFHNJ";//애플리케이션 클라이언트 시크릿값";
+        try {
+            String text = URLEncoder.encode("만나서 반갑습니다.", "UTF-8");
+            String apiURL = "https://openapi.naver.com/v1/language/translate";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            // post request
+            String postParams = "source=ko&target=en&text=" + text;
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+	}
 	
 	
 	@RequestMapping("/chat/coupleChat.do")
